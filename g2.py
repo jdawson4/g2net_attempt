7e59969b7354b9039dda3ad7febdccfdeedceef7
Jacob Dawson <jacob.dawson@me.com>
2022-10-14 18:38:02 -0400

    added some stuff, not done lol

--- g2.py
+++ g2.py
@@ -13,8 +13,34 @@ train_path = data_files_path + "train/"
 test_path = data_files_path + "test/"
 train_dataset = tf.data.Dataset.list_files(train_path+"*.hdf5")
 
-def loss_fn():
-    return 1
+@tf.function
+def loss_fn(y_true, y_pred):
+    # MSE for now
+    tf.reduce_mean(tf.math.square(y_pred - y_true), axis=-1)
+
+# can't read files from an @tf.function
+@tf.py_function
+def preprocess_hdf5(filename):
+    with h5py.File(train_path + tf.compat.as_str_any(filename), 'r') as f:
+        h1_key, l1_key, freq_key = f.keys() # get keys
+
+        # get the three groups:
+        h1_group = f[h1_key]
+        l1_group = f[l1_key]
+        freq_group = f[freq_key]
+        try:
+            for group_key in group.keys():
+                print(np.array(group[group_key]))
+                print(type(np.array(group[group_key])))
+                print(np.array(group[group_key]).shape)
+                print(np.array(group[group_key]).dtype)
+                group2 = group[group_key]
+                #print(f"---->{group2}")
+                for group_key2 in group2.keys():
+                    #print(f"--------->{group2[group_key2]}")
+                    pass
+        except AttributeError:
+            pass
 
 class GWaveDetector(keras.Model):
     def __init__(self, architecture, labels):
@@ -29,11 +55,12 @@ class GWaveDetector(keras.Model):
     
     def train_step(self, data):
         batch_size = tf.shape(data)[0]
+        for i in range(batch_size):
+            hdf5 = preprocess_hdf5(data[i])
         
         # training here:
         with tf.GradientTape() as tape:
-            with h5py.File(tf.as_string(data[0]), 'r') as f:
-                gen_output = self.architecture(data, training=True) # DATA IS NOT THE CORRECT INPUT
+            gen_output = self.architecture(data, training=True)
             loss = loss_fn()
         grads = tape.gradient(loss, self.architecture.trainable_weights)
         self.optimizer.apply_gradients(
